5ca367ea2e28d72a53c38501a9b47feb09db0823
rk-tanvir790 <ktanvir790@gmail.com>
2025-08-16 00:40:19 +0600

    setup react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import MainLayout from './Layout/MainLayout.jsx';
+import Home from './pages/Home.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
   },
 ]);
 
